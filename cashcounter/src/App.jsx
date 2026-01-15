115230d2bee3f8a4078e757d4406c78f3e804b9d
subramanyam <141896120+subbuke@users.noreply.github.com>
2026-01-15 07:03:27 +0000

    running

--- cashcounter/src/App.jsx
+++ cashcounter/src/App.jsx
@@ -3,14 +3,48 @@ import './App.css'
 
 function App() {
   
+  let total 
 
   return (
     <>
     <div><h1 className='title'>Cash counter<span className='sym'>&#8377;</span></h1>
     </div>
-    <div></div>
-     <input type="text" autocomplete="off" name="text" class="input" placeholder="Total amount"></input>
+    <div className='balance'>998008</div>
+    <button type="button" className='btn2'>k</button>
+     <input type="text" autoComplete="off" name="text" className="input" placeholder="Total amount"></input>
       <button type="button">save</button>
+
+  <div className='amt'> 
+    <h2>500*</h2>
+    <input type="text" autocomplete="off" name="five" placeholder="Total amount" className='five'></input>
+      <button type="button" className='fivebtn'>save</button>
+      <h2>200*</h2>
+<input type="text" autocomplete="off" name="text"  placeholder="Total amount" className='two'></input>
+      <button type="button" className='twobtn'>save</button>
+      <h2>100*</h2>
+<input type="text" autocomplete="off" name="text"  placeholder="Total amount" className='one'></input>
+      <button type="button" className='onebtn'>save</button>
+      <h2>50*</h2>
+<input type="text" autocomplete="off" name="text"  placeholder="Total amount" className='fifty'></input>
+      <button type="button" className='fiftybtn'>save</button>
+      <h2>20*</h2>
+<input type="text" autocomplete="off" name="text"  placeholder="Total amount" className='twenty'></input>
+      <button type="button" className='twentybtn'>save</button>
+      <h2>10*</h2>
+<input type="text" autocomplete="off" name="text"  placeholder="Total amount" className='ten'></input>
+      <button type="button" className='tenbtn'>save</button>
+      <h2>5*</h2>
+<input type="text" autocomplete="off" name="text"  placeholder="Total amount" className='fiveru'></input>
+      <button type="button" className='fiverubtn'>save</button>
+      <h2>2*</h2>
+<input type="text" autocomplete="off" name="text"  placeholder="Total amount" className='tworu'></input>
+      <button type="button" className='tworutn'>save</button>
+      <h2>1*</h2>
+<input type="text" autocomplete="off" name="text"  placeholder="Total amount" className='oneru'></input>
+      <button type="button" className='onerubtn'>save</button>
+
+    
+     </div>
     </>
   )
 }
